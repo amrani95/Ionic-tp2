d6664624cb1cca739d828a00c98d102668080528
amrani95 <aymane.amrani@airbus.com>
2020-04-02 14:50:27 +0200

    TU Login

--- src/app/login/login.page.spec.ts
+++ src/app/login/login.page.spec.ts
@@ -4,32 +4,49 @@ import { IonicModule } from '@ionic/angular';
 import { LoginPage } from './login.page';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {LoginService} from '../services/login.service';
 
 describe('LoginPage', () => {
   let component: LoginPage;
   let fixture: ComponentFixture<LoginPage>;
-
+  // tslint:disable-next-line:prefer-const
+  let httpTestingController: HttpTestingController;
+  const mockData = [
+    {
+      id: 1,
+      name: 'Le nom de Bret',
+      username: 'Bret',
+      email: 'Sincere@april.biz'
+    }
+  ];
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ LoginPage ],
-      imports: [IonicModule.forRoot(), CommonModule, HttpClientModule,
-        FormsModule]
+      imports: [IonicModule.forRoot(), CommonModule, HttpClientTestingModule,
+        FormsModule],
+      providers: [LoginService]
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    httpTestingController = TestBed.get(HttpTestingController);
   }));
 
 
   it('Login', () => {
     expect(component.user).toBeUndefined();
-    component.id = 'Bret';
+    const username = 'Bret';
+    component.id = username;
     component.mdp = 'monMdp';
     component.login();
+    const req = httpTestingController.expectOne('https://jsonplaceholder.typicode.com/users?username=' + username);
+    req.flush(mockData);
+    httpTestingController.verify();
     expect(component.user).toBeDefined();
-    expect(component.user.id).toBe('Bret');
+    expect(component.user.nom).toBe(mockData[0].name);
 
   });
 });
